d3602e735bba751828205c959087e3135433b302
Potamitisn <nearchosp@outlook.com>
2021-10-28 03:16:01 +0200

    Data Exploration & Pipeline Notebooks, Folders for organization & Updates for compute/implementations/helpers/data_cleaner

--- project1/scripts/compute.py
+++ project1/scripts/compute.py
@@ -4,33 +4,54 @@ def compute_error(y, tx, w):
     """
     Computes the error
     """
-    error = y- tx @ w
-    
+    error =y - (tx @ w)
     return error
 
 def compute_mse_loss(error):
+    """
+    Computes the mean squared error for a given error
+    """
     mean_squared_error = np.mean(error ** 2) / 2
     return mean_squared_error
 
 def compute_gradient(tx, error):
-    
-    gradient = -(tx @ error) / len(error)
+    """
+    Computes the gradient for a given error 
+    """
+    gradient = -tx.T.dot(error) / len(error)
     
     return gradient
 
-def sigmoid(t):
+def compute_sigmoid(t):
+    """
+    Computes the output of the sigmoid function for a given input
+    """
+    t = np.clip(t, -8,8)
     sigmoid_t = 1 / (1 + np.exp(-t))
     return sigmoid_t
 
-#from course
 def compute_logistic_loss(y, tx, w):
-    predictions = sigmoid(tx @ w)
+    """
+    Computes the logistic loss of a model
+    """
+    predictions = compute_sigmoid(tx @ w)
     neg_losses_per_datapoint = y * np.log(predictions) + (1-y) * np.log(1-predictions)
     loss = - neg_losses_per_datapoint.sum()
-    return losses
+    return loss
 
-#from course
 def compute_logistic_gradient(y, tx, w):
-    predictions = sigmoid(tx @ w)
-    grad = tx @ (predictions - y)
+    """
+    Computes the logistic gradient of a model
+    """
+    predictions = compute_sigmoid(tx @ w)
+    grad = tx.T @ (predictions - y)
     return grad
+
+def compute_leaderboard_score(y_true,y_pred):
+    """
+    Helper function estimating the categorical accuracy on the leaderscore
+    """
+    N_tot = y_pred.shape[0]
+    N_true = len(np.where(y_pred == y_true)[0])
+    categorical_acuracy = N_true/N_tot
+    return categorical_acuracy
--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -1,4 +1,4 @@
-from tools import *
+from compute import *
 from proj1_helpers import batch_iter
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
@@ -6,14 +6,18 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     Linear regression using gradient descent
     """
     w = initial_w
-    
-    for iter in range(max_iters):
+    for iter_ in range(max_iters):
         
-        error = compute_error(y, tx, w)        
+        error = compute_error(y, tx, w) 
+        loss = compute_mse_loss(error)
         gradient = compute_gradient(tx, error)
-        w -= gamma * gradient
+        w = w - gamma * gradient
         
-    loss = compute_mse_loss(error)
+        if iter_ % 250 == 0:
+                print("Current iteration :%d, loss= %.4f" %(iter_, loss)) 
+                
+        if loss < 0.1 :
+            break
     return w, loss
        
         
@@ -22,16 +26,21 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     """
     Linear regression using stochastic gradient descent
     """
-    w = intial_w
+    w = initial_w
     
-    for iter in range(max_iters):
+    for iter_ in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
             
-            error = compute_error(y, tx, w)           
-            gradient = compute_gradient(tx, error)
+            error = compute_error(y_batch, tx_batch, w)  
+            loss = compute_mse_loss(error)
+            gradient = compute_gradient(tx_batch, error)
             w -= gamma * gradient
             
-    loss = compute_mse_loss(error)
+            if iter_ % 250 == 0:
+                print("Current iteration :%d, loss= %.4f" %(iter_, loss)) 
+            
+            if loss < 0.1:
+                break
     return w, loss
           
     
@@ -40,7 +49,6 @@ def least_squares(y, tx):
     """
     Least squares regression using normal equations
     """
-    #From least_squares_mathias
     w = np.linalg.solve(tx.T @ tx, tx.T @ y)   #system of type A*x = b solve(A,b)    
     error = compute_error(y, tx, w)
     loss = compute_mse_loss(error)
@@ -51,7 +59,6 @@ def ridge_regression(y, tx, lambda_):
     """
     Ridge regression using normal equations
     """
-    #from ridge_regression_matthias
     w = np.linalg.solve(tx.T @ tx + (lambda_*2*len(y)) * np.identity(tx.shape[1]), tx.T @ y)   #system of type A*x = b solve(A,b)    
     error = compute_error(y, tx, w)
     loss = compute_mse_loss(error)
@@ -65,11 +72,14 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """
     w = initial_w
     
-    for iter in range(max_iters):
+    for iter_ in range(max_iters):
         
         loss = compute_logistic_loss(y, tx, w)
         gradient = compute_logistic_gradient(y, tx, w)
         w -= gamma * gradient
+        
+        if iter_ % 250 == 0:
+                print("Current iteration :%d, loss= %.4f" %(iter_, loss)) 
     
     return w, loss
         
@@ -81,10 +91,13 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """ 
     w = initial_w
     
-    for iter in range(max_iters):
+    for iter_ in range(max_iters):
         
-        loss = compute_logistic_loss(y, tx, w) + lambda_ / 2 * np.squeeze(w.T @ w) #Last part might need x2
-        gradient = compute_logistic_gradient(y, tx, w) + lambda_ * w #Last part might need x2
-        w -= gamma * gradient    
+        loss = compute_logistic_loss(y, tx, w) + lambda_  * np.squeeze(w.T @ w) #Last part might need x2
+        gradient = compute_logistic_gradient(y, tx, w) + 2*lambda_ * w #Last part might need x2
+        w -= gamma * gradient   
+        
+        if iter_ % 250 == 0:
+                print("Current iteration :%d, loss= %.4f" %(iter_, loss)) 
     
     return w, loss
